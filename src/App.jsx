54f06bd5d30c848999582d9fe6a96c49bfc8b584
ksuju <139477852+ksuju@users.noreply.github.com>
2025-02-24 11:24:54 +0900

    fix: 컨플릭트 수정정

--- src/App.jsx
+++ src/App.jsx
@@ -6,28 +6,26 @@ import Login from './pages/Login'
 import MyPage from './pages/MyPage'
 import SignUp from './pages/SignUpPage'
 import LoginPet from './pages/LoginPet'
-import Layout from './layout/Layout.jsx'
+// import Layout from './layout/Layout.jsx'
 import SocialConnect from './pages/SocialConnectPage'
 
-import ReportMissingPet  from './pages/ReportMissingPet'
 import { useAuthStore } from './stores/useAuthStore'
 import './index.css';
 
 const App = () => {
 
   return (
-      <Routes>
-        <Route element={<Layout />}>
-            <Route path="/" element={<Main />} />
-            <Route path="/map" element={<MainMapView />} />
-            <Route path="/login" element={<Login />} />
-            <Route path="/login-pet" element={<LoginPet />} />
-            <Route path="/sign-up" element={<SignUp />} />
-            <Route path="/mypage" element={<MyPage />} />
-            <Route path="/find" element={<ReportMissingPet/>} />
-            <Route path="/connect" element={<SocialConnect/>} />
-        </Route>
-      </Routes>
+    <Routes>
+      <Route element={<Layout />}>
+        <Route path="/login" element={<Login />} />
+        <Route path="/login-pet" element={<LoginPet />} />
+        <Route path="/sign-up" element={<SignUp />} />
+        <Route path="/mypage" element={<MyPage />} />
+        <Route path="/connect" element={<SocialConnect />} />
+        <Route path="/" element={<Map />} />
+        <Route path="/login" element={<Login />} />
+      </Route>
+    </Routes>
   );
 };
 
