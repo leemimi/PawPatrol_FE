2cc7bd3b7a40f4f0363d2a423c9d5a88a073be99
backnback <garam8796@naver.com>
2025-02-25 15:22:18 +0900

    병합

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,6 @@ import { useEffect } from 'react'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import Layout from './layout/Layout'
 import Map from './pages/Map'
-import Login from './pages/Login'
 import MyPage from './pages/MyPage'
 import SignUp from './pages/SignUpPage'
 import LoginPet from './pages/LoginPet'
@@ -16,14 +15,12 @@ const App = () => {
   return (
     <Routes>
       <Route element={<Layout />}>
-        <Route path="/login" element={<Login />} />
         <Route path="/login-pet" element={<LoginPet />} />
         <Route path="/sign-up" element={<SignUp />} />
         <Route path="/mypage" element={<MyPage />} />
         <Route path="/connect" element={<SocialConnect />} />
         <Route path="/" element={<Map />} />
         <Route path="/protection" element={<Protection />} />
-        <Route path="/login" element={<Login />} />
       </Route>
     </Routes>
   );
