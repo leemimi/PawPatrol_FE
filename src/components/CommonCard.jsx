025609b7dd6d759c068f04086778ee43d24ff75d
junghyun13 <ljh041314@gmail.com>
2025-02-28 17:45:04 +0900

    수정

--- src/components/CommonCard.jsx
+++ src/components/CommonCard.jsx
@@ -15,7 +15,7 @@ console.log('item.pet.animalType:', item.pet.animalType);
 
 
     // 반려동물 상태 텍스트 변환
-    const getStatusText = (status) => {
+    const getStatusText = (status) => { //수정 
         switch(status) {
             case 'FINDING': return '찾는중';
             case 'FOSTERING': return '임보 중';
