d9d270e58e887bae638495744d5e17fee36550e0
ksuju <139477852+ksuju@users.noreply.github.com>
2025-03-14 16:40:41 +0900

    chore.

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -86,7 +86,7 @@ define(['./workbox-19205ddb'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.rg1pcbsbaeg"
+    "revision": "0.8bdt9j5rk1g"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
